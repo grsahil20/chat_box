be0a4ed9d8d6e13db22e70a6794b1a2bb061bbdf
Sahil Grover <gr.sahil20@gmail.com>
2017-04-07 13:38:51 +0800

    converter service finished

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,15 @@ class ChatsController < ApplicationController
   def create
     respond_to do |format|
       format.js do
-        @chat_message = params[:message]
+        converter_service = UserDialect::ConverterService.new(
+          user_name: current_user,
+          dialect: current_dialect,
+          message: params[:message]
+        )
+
+        converter_service.run
+        @chat_message = converter_service.converted_message
+
         @user = current_user
         @created_at = Time.now
       end
--- app/services/user_dialect/converter_service.rb
+++ app/services/user_dialect/converter_service.rb
@@ -1,22 +1,44 @@
+require 'nokogiri'
+require 'open-uri'
+
 module UserDialect
   class ConverterService
 
+    BASE_URL = "http://www.degraeve.com/cgi-bin/babel.cgi?"
+    DIALECTS = {
+      'yoda' => 'yoda',
+      'valley_girl' => 'valley',
+      'binary_code' => 'binary',
+    }
     attr_accessor :converted_message
 
     def initialize(user_name:, dialect:, message: )
+      p dialect
       @user_name = user_name
-      @dialect   = dialect || UserDialect::StorageService.get_user_dialect(current_user)
+      @dialect   = (dialect || UserDialect::StorageService.get_user_dialect(current_user)).gsub(' ', '_').downcase
       @message   = message
     end
 
     def run
-      return converted_message
+      create_link
+      return convert_message
     end
 
     private
 
-    def converted_message
-      @converted_message ||= @message
+    def create_link
+      @link = UserDialect::ConverterService::BASE_URL + link_params.to_param
+    end
+
+    def link_params
+      {
+        d: UserDialect::ConverterService::DIALECTS[@dialect],
+        w: @message,
+      }
+    end
+
+    def convert_message
+      @converted_message ||= Nokogiri::HTML(open @link).css('body table p').first.content.strip
     end
   end
 end
